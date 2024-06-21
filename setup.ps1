16ceec87e7d014ed7c0343fc336b156c6b111034
xmt3061123 <71756356+xmt3061123@users.noreply.github.com>
2024-06-21 10:22:32 +0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -2,39 +2,44 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
 # 定义下载和安装函数
-function Download-And-Install {
+$scriptBlock = {
     param (
         [string]$url,
         [string]$fileName,
         [string]$installArgs = ""
     )
 
-    $tempPath = Join-Path -Path $env:TEMP -ChildPath $fileName
-    try {
-        Write-Output "Downloading $url to $tempPath"
-        Invoke-WebRequest -Uri $url -OutFile $tempPath -ErrorAction Stop
-        Write-Output "Starting installation of $fileName"
-        Start-Process -FilePath $tempPath -ArgumentList $installArgs -Verb RunAs -Wait -ErrorAction Stop
-    }
-    catch {
-        Write-Error "Failed to download or install ${fileName}: $_"
-    }
-    finally {
-        if (Test-Path $tempPath) {
-            Remove-Item $tempPath -Force
-            Write-Output "${fileName} has been removed from temp"
+    function Download-And-Install {
+        param (
+            [string]$url,
+            [string]$fileName,
+            [string]$installArgs = ""
+        )
+
+        $tempPath = Join-Path -Path $env:TEMP -ChildPath $fileName
+        try {
+            Write-Output "Downloading $url to $tempPath"
+            Invoke-WebRequest -Uri $url -OutFile $tempPath -ErrorAction Stop
+            Write-Output "Starting installation of $fileName"
+            Start-Process -FilePath $tempPath -ArgumentList $installArgs -Verb RunAs -Wait -ErrorAction Stop
+        }
+        catch {
+            Write-Error "Failed to download or install ${fileName}: $_"
+        }
+        finally {
+            if (Test-Path $tempPath) {
+                Remove-Item $tempPath -Force
+                Write-Output "${fileName} has been removed from temp"
+            }
         }
     }
+
+    Download-And-Install -url $using:url -fileName $using:fileName -installArgs $using:installArgs
 }
 
 # 使用后台作业并行下载和安装
-$job1 = Start-Job -ScriptBlock {
-    Download-And-Install -url "https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi" -fileName "chromeremotedesktophost.msi"
-}
-
-$job2 = Start-Job -ScriptBlock {
-    Download-And-Install -url "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -fileName "chrome_installer.exe" -installArgs "/install"
-}
+$job1 = Start-Job -ScriptBlock $scriptBlock -ArgumentList "https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi", "chromeremotedesktophost.msi", ""
+$job2 = Start-Job -ScriptBlock $scriptBlock -ArgumentList "https://dl.google.com/chrome/install/latest/chrome_installer.exe", "chrome_installer.exe", "/install"
 
 # 等待所有作业完成
 Wait-Job -Job $job1, $job2
